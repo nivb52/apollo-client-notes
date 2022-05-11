1a049887f9ee0826977788ef711669967f1e1e8c
nivb52 <nivb52@gmail.com>
2022-05-11 15:40:27 +0300

    edit note and show (tnx to apollo cache refresher)

---
+++ client/src/EditNote.js
@@ -0,0 +1,55 @@
+import { gql, useQuery, useMutation } from "@apollo/client";
+import { Spinner } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+import { UiEditNote } from "./shared-ui";
+
+const UPDATE_NOTE_MUTATION = gql`
+   mutation UpdateNote($id: String!, $content: String!) {
+        updateNote(id: $id, content: $content) {
+            successful,
+            note {
+                id
+                content
+            }
+        }
+    }
+`;
+
+const NOTE_QUERY = gql`
+    query GetNote($id: String!) {
+        note(id: $id) {
+            id
+            content
+        }
+    }
+`;
+
+
+export function EditNote() {
+    const { noteId } = useParams();
+    let { data } = useQuery(NOTE_QUERY, {
+        variables: {
+            id: noteId
+        }
+    });
+    const [updateNote, noteResponse] = useMutation(UPDATE_NOTE_MUTATION);
+
+    if (noteResponse && noteResponse.loading && !noteResponse.data)
+      return <Spinner />;
+    else if (noteResponse?.error)
+      return <Heading> Could not load note. </Heading>;
+    
+
+    return (
+      <UiEditNote
+        onSave={(newContent) => {
+          updateNote({ variables: { id: noteId, content: newContent } });
+        }}
+        note={
+          noteResponse?.data?.successful
+            ? noteResponse.data?.note
+            : data?.note 
+        }
+      />
+    );
+}
